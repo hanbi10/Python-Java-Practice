652baa658bd1a1653971a450e25befa5f8e4dc07
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-11-08 20:26:56 +0900

    .

---
+++ python/1302.py
@@ -0,0 +1,21 @@
+N = int(input())
+ 
+count = {}
+for _ in range(N) :
+    title = input()
+ 
+    if title not in count :
+        count[title] = 0
+    count[title] += 1
+ 
+max_value = 0
+max_title = ""
+for key, value in count.items() :
+    if max_value < value :
+        max_value = value
+        max_title = key
+    elif max_value == value :
+        if max_title > key :
+            max_title = key
+ 
+print(max_title)
